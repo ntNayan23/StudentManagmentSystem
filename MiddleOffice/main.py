1b74d230bb1b1947e2aeaf28c6c18e76bf44f138
lalitarakshak <lalitar572.com>
2022-04-11 09:24:20 +0530

    my first program lalita

---
+++ MiddleOffice/main.py
@@ -0,0 +1,5 @@
+print("")
+name = input("enter the name of student")
+age = input("enter the age of student")
+phoneNumber = input("enter the phone number of student")
+print("Name="+name , "age="+age , "phoneNumber="+phoneNumber)
