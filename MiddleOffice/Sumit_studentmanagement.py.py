d1ceff7b2e7a1a1b9a2757a2a57656a7215e40a7
nayan thakre <nayanthakre379@gmail.com>
2022-04-11 13:20:35 +0530

    remove folder

--- MiddleOffice/Sumit_studentmanagement.py.py
+++
@@ -1,4 +0,0 @@
-Name = input("enter the name of students")
-Age = input("enter the name of age")
-Mobnumber = input("enter the name of mobnumber")
-print('Name = name, Age = age, Phone number = mobnumber')
