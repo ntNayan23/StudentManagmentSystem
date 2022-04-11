3cbe61e51304a03b3b9551ecd4eb708dcef33aa1
sumit <sumitparate30@gmail.com>
2022-04-11 09:35:16 +0530

    my first programme sumit

---
+++ MiddleOffice/Sumit_studentmanagement.py.py
@@ -0,0 +1,4 @@
+Name = input("enter the name of students")
+Age = input("enter the name of age")
+Mobnumber = input("enter the name of mobnumber")
+print('Name = name, Age = age, Phone number = mobnumber')
