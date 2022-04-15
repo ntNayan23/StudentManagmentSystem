6839037243bc1b8499a4ee7810a42fd77b3f2e5b
sumit <sumitparate30@gmail.com>
2022-04-15 07:04:34 +0530

    updating menu

--- MiddleOffice/Sumit_StudentMangement.py
+++ MiddleOffice/Sumit_StudentMangement.py
@@ -1,18 +1,37 @@
-print('''------------------------ \nGovernment Polytechnic \n------------------------''' )
-print(''' 1.Add New Student\n 2.Delete Student\n 3.Search Student''')
-choice =  int(input("Enter You choice :- "))
-if(choice == 1):
-    print("Add New Student")
+def add():
     name = input("Enter Name :- ")
     age = int(input("Enter Age :- "))
-    mob_number = int(input("Enter mob_number :-"))
+    phone_number = int(input("Enter Phone Number :-"))
     print("New Student is added")
-elif(choice == 2):
+
+def delete():
     name = input("Enter Name of Student:- ")
     print("Delete Student")
-elif(choice == 3):
-    name = input("Enter Name of Student:- ")
-    print("It should display student Name",name)
 
-else:
-    print("Invalid Choice")
+def search():
+    name = input("Enter Name of Student:- ")
+    print("It should display student Name", name)
+
+def header():
+    print('''********* \nWainganga \n***********''')
+
+
+def choice():
+    print(''' 1.Add New Student\n 2.Delete Student\n 3.Search Student''')
+    choice = int(input("Enter You choice :- "))
+    return choice
+
+if __name__=="__main__":
+    cho = choice()
+    if cho == 1:
+        header()
+        add()
+    elif cho == 2:
+        header()
+        delete()
+    elif cho == 3:
+        header()
+        search()
+    else:
+        print("Invalid Choice")
+
